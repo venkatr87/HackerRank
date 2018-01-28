b2a53d51161b29d2b57ea8a0e0136b1e6d085331
venkatr87 <35232364+venkatr87@users.noreply.github.com>
2018-01-28 14:23:14 +0530

    Modified Count pre defined to user defined

--- birthdaycake candles.py
+++ birthdaycake candles.py
@@ -3,7 +3,11 @@
 import sys
 def birthdayCakeCandles(n, ar):
 	max_height = max (ar)
-	return ar.count(max_height)
+	count = 0
+	for i in ar:
+		if (i == max_height):
+			count += 1
+	return count
 	
 n = int(raw_input().strip())
 ar = map(int, raw_input().strip().split(' '))
